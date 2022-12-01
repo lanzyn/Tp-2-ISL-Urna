3e1e868b3bd42c6208b847235289d908870219c7
Lanzyn <alan.a.reis@ufv.br>
2022-11-30 23:35:40 -0300

    ....

--- tb.v
+++ tb.v
@@ -1,10 +1,11 @@
-`include "Urna.v"
-`default_nettype none
-
+`include "code.v"
 module tb_Urna();
 reg tb_clk;
 reg tb_finish;
-reg tb_digit;
+reg tb_digit0;
+reg tb_digit2;
+reg tb_digit1;
+reg tb_digit3;
 reg tb_swap;
 reg tb_valid;
 
@@ -18,7 +19,10 @@ Urna urnatb
     .finish(tb_finish),
     .clk(tb_clk),
     .swap(tb_swap),
-    .digit(tb_digit),
+    .digit0(tb_digit0),
+    .digit2(tb_digit2),
+    .digit1(tb_digit1),
+    .digit3(tb_digit3),
     .valid(tb_valid),
     .VoteStatus(tb_VoteStatus),
     .contadorC1(tb_TotalC1),
@@ -29,19 +33,67 @@ Urna urnatb
 localparam CLK_PERIOD = 10;
 always #(CLK_PERIOD/2) tb_clk=~tb_clk;
 
+initial
+begin
+    $monitor ("time = %d, finish = %b, candidate_1 = %b, candidate_2 = %b, candidate_3 = %b, vote_over = %b, result_1 = %3d, result_2 = %3d, result_3 = %3d,\n",
+    $time, tb_finish, tb_digit0, tb_digit1, tb_digit2, tb_finish, tb_TotalC1, tb_TotalC2, tb_TotalNull,);
+
+    
+    tb_finish = 1'b0;
+    tb_digit0 = 1'b0;
+    tb_digit1 = 1'b0;
+    tb_digit2 = 1'b0;
+    tb_digit3 = 1'b0;
+    tb_valid = 1'b0;
+    tb_swap = 1'b0;
+
+    #20 tb_digit0 = 1'b0;
+    #10 tb_digit1 = 1'b0;
+    #10 tb_digit2 = 1'b1;
+    #10 tb_digit3 = 1'b0;
+    #10 tb_valid = 1'b1;
+
+    #20 tb_digit0 = 1'b0;
+    #10 tb_digit1 = 1'b0;
+    #10 tb_digit2 = 1'b1;
+    #10 tb_digit3 = 1'b0;
+    #10 tb_valid = 1'b1;
+
+    #20 tb_digit0 = 1'b0;
+    #10 tb_digit1 = 1'b0;
+    #10 tb_digit2 = 1'b0;
+    #10 tb_digit3 = 1'b1;
+    #10 tb_valid = 1'b1;
+
+    #20 tb_digit0 = 1'b0;
+    #10 tb_digit1 = 1'b0;
+    #10 tb_digit2 = 1'b1;
+    #10 tb_digit3 = 1'b1;
+    #10 tb_valid = 1'b1;
+
+    #20 tb_digit0 = 1'b0;
+    #10 tb_digit1 = 1'b0;
+    #10 tb_digit2 = 1'b0;
+    #10 tb_digit3 = 1'b1;
+    #10 tb_valid = 1'b1;
+
+    #20 tb_digit0 = 1'b0;
+    #10 tb_digit1 = 1'b0;
+    #10 tb_digit2 = 1'b1;
+    #10 tb_digit3 = 1'b1;
+    #10 tb_valid = 1'b1;
+
+
+    #30 tb_finish = 1'b1;
+    #50 tb_finish = 1'b1;                                    //reset when the voting process is over 
+    
+    //use $finish for simulators other than modelsim
+    #60 $stop;                                          // use $stop instead of $finish to keep modelsim simulator open 
+end
+
 initial begin
     $dumpfile("tb_Urna.vcd");
     $dumpvars(0, tb_Urna);
 end
 
-initial begin
-    tb_finish = 1'b0;
-    tb_digit = 4'b0010;
-    tb_valid = 1'b1;
-    tb_digit = 4'b0010;
-    tb_valid = 1'b1;
-    $display("Status: %d", tb_VoteStatus);
-end
-
 endmodule
-`default_nettype wire
